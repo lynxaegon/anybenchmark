9bef9d68d8f65c023ce5ea18a2a165852ba36976
Andrei Vaduva <andrei.vaduva@gmail.com>
2022-03-18 19:11:58 +0200

    Fixed ws test server

--- tests/ws.js
+++ tests/ws.js
@@ -3,8 +3,6 @@ const ws = new WebSocket.Server({ port: 8080 })
 
 ws.on('connection', function connection(ws) {
     ws.on('message', function message(data) {
-        // console.log('received: %s', data);
+        ws.send('hello');
     });
-
-    ws.send('something');
 });
