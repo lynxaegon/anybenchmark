49c4ad5e68119f25657915b65fff095840e1da65
Andrei Vaduva <andrei.vaduva@gmail.com>
2022-03-18 18:55:20 +0200

    Fixed protocol anysocket ip and port

--- protocols/anysocket.js
+++ protocols/anysocket.js
@@ -4,10 +4,11 @@ let AnySocket = require('anysocket');
 module.exports = class WSProtocol extends ProtocolBase {
     constructor(url, options) {
         super(url, options)
-
         this.socket = new AnySocket();
         this.peer = null;
-        this.socket.connect("ws", "127.0.0.1",8080).then((peer) => {
+
+        url = url.split(":");
+        this.socket.connect("ws", url[0], url[1]).then((peer) => {
             this.peer = peer;
             this.emit("open")
         }).catch((err) => {
